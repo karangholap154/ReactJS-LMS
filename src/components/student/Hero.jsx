e960161cf9fdcc929832ffa3aac29e692f206d39
karann001 <karangholap154@gmail.com>
2025-02-25 22:14:34 +0530

    updated Hero component and Home page with new design and layout

--- src/components/student/Hero.jsx
+++ src/components/student/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Hero = () => {
   return (
-      <div>
-          <h1>Hero</h1>
+    <div className='flex flex-col items-center justify-center w-full pt-20 text-center md:pt-36 px-7 md:px-0 space-y-7 bg-gradient-to-b from-cyan-100/70'>
+      <h1 className='relative max-w-3xl mx-auto font-bold text-gray-800 md:text-home-heading-large text-home-heading-small'>
+        Empower your future with the courses designed to <span className='text-blue-600'> fit your choice.</span>
+        <img src={assets.sketch} alt="sketch" className='absolute right-0 hidden md:block -bottom-7' />
+      </h1>
+
+      <p className='hidden max-w-2xl mx-auto text-gray-500 md:block'>
+        We bring together world-class instructors, interactive content, and a supportive community to help you achieve your personal and professional goals.
+      </p>
+
+      <p className='max-w-sm mx-auto text-gray-500 md:hidden'>
+      We bring together world-class instructors to help you achieve your personal and professional goals.
+      </p>
     </div>
   )
 }
