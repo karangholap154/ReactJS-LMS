0cf90370558747846f86149136e2750201b45be7
karann001 <karangholap154@gmail.com>
2025-03-01 20:03:46 +0530

    updated navbar and testimonials section with educator functionality

--- src/components/student/TestimonialsSection.jsx
+++ src/components/student/TestimonialsSection.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { assets, dummyTestimonial } from '../../assets/assets'
 
 const TestimonialsSection = () => {
     return (
-        <div>
-            <h1>Testimonials</h1>
+        <div className='px-8 pb-14 md:px-0'>
+            <h2 className='text-3xl font-medium text-gray-800'>Testimonials</h2>
+            <p className='mt-3 text-gray-500 md:text-base'>Hear from our learners as they share their journeys of transformation, success, and how our <br /> platform has made a difference in their lives.</p>
+            <div className='grid gap-8 grid-cols-auto mt-14'>
+                {dummyTestimonial.map((testimonial, index) => (
+                    <div key={index} className='pb-6 text-sm text-left bg-white border rounded shadow border-grayrounded-lg-[0px_4px_15px_0px] overflow-hidden shadow-black/5'>
+                        <div className='flex items-center gap-4 px-5 py-4 bg-gray-500/10'>
+                            <img className='w-12 h-12 rounded-full' src={testimonial.image} alt={testimonial.name} />
+                            <div>
+                                <h1 className='text-lg font-medium text-gray-800'>{testimonial.name}</h1>
+                                <p className='text-gray-800/80'>{testimonial.role}</p>
+                            </div>
+                        </div>
+                        <div className='p-5 pb-7'>
+                            <div className='flex gap-0.5'>
+                                {[...Array(5)].map((_, i) => (
+                                <img className='h-5' key={i} src={i < Math.floor(testimonial.rating) ? assets.star : assets.star_blank} alt="star" />
+                                ))}
+                            </div>
+                            <p className='mt-5 text-gray-500'>{testimonial.feedback}</p>
+                        </div>
+                        <a href="#" className='px-5 text-blue-500 underline'>Read More</a>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
+import TestimonialsSection from '../../components/student/TestimonialsSection'
 
 const Home = () => {
   return (
       <div className='flex flex-col items-center text-center space-y-7'>
         <Hero />
         <Companies />
-        <CoursesSection/>
+        <CoursesSection />
+        <TestimonialsSection/>
     </div>
   )
 }
