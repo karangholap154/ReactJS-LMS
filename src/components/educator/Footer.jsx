af92a664a8bfae282573b541147804b83d544467
karann001 <karangholap154@gmail.com>
2025-03-05 18:39:30 +0530

    updated educator components and layout

--- src/components/educator/Footer.jsx
+++ src/components/educator/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Edu Footer</h1>
-        </div>
+        <footer className='flex flex-col items-center justify-between w-full px-8 text-left border-t md:flex-row-reverse'>
+            <div className='flex items-center gap-4'>
+                <img src={assets.logo} alt="logo" className='hidden w-20 md:block' />
+                <div className='hidden w-px md:block h-7 bg-gray-500/60'></div>
+                <p className='py-4 text-xs text-center text-gray-500 md:text-sm'>Copyright 2025 © LMS. All Right Reserved.</p>
+            </div>
+            <div className='flex items-center gap-3 max-md:mt-4'>
+                <a href="#">
+                    <img src={assets.facebook_icon} alt="facebook" />
+                </a>
+                <a href="#">
+                    <img src={assets.twitter_icon} alt="twitter" />
+                </a>
+                <a href="#">
+                    <img src={assets.instagram_icon} alt="instagram" />
+                </a>
+            </div>
+        </footer>
     )
 }
 
--- src/components/educator/Sidebar.jsx
+++ src/components/educator/Sidebar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { assets } from '../../assets/assets'
+import {AppContext} from '../../context/AppContext'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-    return (
-        <div>
-            <h1>My Sidebar</h1>
+
+    const {isEducator} = useContext(AppContext)
+
+    const menuItems = [
+        { name: 'Dashboard', path: '/educator', icon: assets.home_icon },
+        { name: 'Add Course', path: '/educator/add-course', icon: assets.add_icon },
+        { name: 'My Courses', path: '/educator/my-courses', icon: assets.my_course_icon },
+        { name: 'Students Enrolled', path: '/educator/student-enrolled', icon: assets.person_tick_icon },
+    ];
+
+    return isEducator && (
+        <div className='flex flex-col w-16 min-h-screen py-2 text-base border-r border-gray-500 md:w-64'>
+            {menuItems.map((item) => (
+                <NavLink
+                    to={item.path}
+                    key={item.name}
+                    end={item.path === '/educator'}
+                    className={({isActive}) => `flex items-center md:flex-row flex-col md:justify-start justify-center py-3.5 md:px-10 gap-3 ${isActive ? 'bg-indigo-50 border-r-[6px] border-indigo-500/90' : 'hover:bg-gray-100/90 border-r-[6px] border-white hover:border-gray-100/90'}`}>
+                    <img src={item.icon} alt="" className='w-6 h-6' />
+                    <p className='hidden text-center md:block'>{item.name}</p>
+                </NavLink>
+            ))}
         </div>
     )
 }
