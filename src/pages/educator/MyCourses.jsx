0ac205b6fff95f758151a1f22b7239e923d44fda
karann001 <karangholap154@gmail.com>
2025-03-06 18:51:53 +0530

    updated MyCourses component to fetch educator courses and display them in a table

--- src/pages/educator/MyCourses.jsx
+++ src/pages/educator/MyCourses.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
 
 const MyCourses = () => {
-  return (
-    <div>MyCourses</div>
-  )
+
+  const { currency, allCourses } = useContext(AppContext)
+  
+  const [courses, setCourses] = useState(null)
+
+  const fetchEducatorCourses = async () => {
+    setCourses(allCourses)
+  }
+
+  useEffect(() => {
+    fetchEducatorCourses()
+  }, [])
+
+  return courses ? (
+    <div className='flex flex-col items-start justify-between h-screen p-4 pt-8 pb-0 mb-5 overflow-auto md:p-8 md:pb-0'>
+      <div className='w-full'>
+        <h2 className='pb-4 text-lg font-medium'>My Courses</h2>
+        <div className='flex flex-col items-center w-full max-w-4xl overflow-hidden bg-white border rounded-md border-gray-500/20'>
+          <table className='w-full overflow-hidden table-fixed md:table-auto'>
+            <thead className='text-sm text-left text-gray-900 border-b border-gray-500/20'>
+              <tr>
+                <th className='px-4 py-3 font-semibold truncate'>All Courses</th>
+                <th className='px-4 py-3 font-semibold truncate'>Earnings</th>
+                <th className='px-4 py-3 font-semibold truncate'>Students</th>
+                <th className='px-4 py-3 font-semibold truncate'>Published On</th>
+              </tr>
+            </thead>
+            <tbody className='text-sm text-gray-500'>
+              {courses.map((course) => (
+                <tr key={course._id} className="border-b border-gray-500/20">
+                  <td className='flex items-center py-3 pl-2 space-x-3 truncate md:px-4 md:pl-4'>
+                    <img src={course.courseThumbnail} alt="course image" className='w-16'/>
+                    <span className='hidden truncate md:block'>{course.courseTitle}</span>
+                  </td>
+                  <td className='px-4 py-3'>{currency} {Math.floor(course.enrolledStudents.length * (course.coursePrice - course.discount * course.coursePrice / 100))}</td>
+                  <td className='px-4 py-3'>{course.enrolledStudents.length}</td>
+                  <td className='px-4 py-3'>{new Date(course.createdAt).toLocaleDateString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  ) : <Loading/>
 }
 
 export default MyCourses
